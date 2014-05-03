2de5b8dae84fd2e6d4f30320e1152ba2e707e977
KDercksen <mail@koendercksen.com>
2014-05-04 00:12:17 +0200

    Fix install script, add muttrc and remove Xresources and bash_profile

--- .bash_profile
+++
@@ -1,3 +0,0 @@
-# Make sure .bashrc gets sourced on terminal startup 
-# in order to print awesome fortune cookie
-source .bashrc
--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Install dotfiles by backing up any old ones and creating symlinks to the ones in this repository
-dir=$(dirname $0)
-olddir=$HOME/dotfiles.old/
+set -e
 
-# First create dotfiles.old in homedir
-mkdir -pv $olddir
+FILES=".bashrc .gitconfig .muttrc .vimrc .vim"
+BACKUPDIR="$HOME/dotfiles.old"
 
-# Move any existing dotfiles in ~ into olddir and create symlinks to new dotfiles
-for file in $(ls -a1 $dir | grep -xv "\.*\($\|git\(modules\)\+$\)\|README.md\|install.sh")
+mkdir -pv $BACKUPDIR
+
+for file in $FILES
 do
     if [ -e $HOME/$file ]; then
-        mv -v $HOME/$file $olddir$file
+        mv -v $HOME/$file $BACKUPDIR/$file
     fi
-    ln -sv $dir/$file $HOME/$file
+    ln -sv $PWD/$file $HOME/$file
 done
 
 echo "All done!"
